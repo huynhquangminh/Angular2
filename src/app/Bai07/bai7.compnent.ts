437fe1fd107cd688f6cddf90d2a9ab88be7971a2
Minh Huynh <huynhtony08@gmail.com>
2017-10-31 21:29:14 +0700

    angular 2 , use even

--- src/app/Bai07/bai7.compnent.ts
+++ src/app/Bai07/bai7.compnent.ts
@@ -1,9 +1,15 @@
 import {Component} from '@angular/core';
 
 @Component ({
-    selector:'bai07-demo'
+    selector:'bai07-demo',
+    templateUrl :'bai7.component.html',
+    
 })
 export class bai7Component
 {
-    
+    public dem :number = 1;
+    OnClickBtn(value)
+    {
+        console.log(value);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import {bai3Component} from './Bai3/bai3.component';
 import {bai5Component} from './Bai05/bai5.component';
 import {bai6Component} from './Bai06/bai6.component';
+import {bai7Component} from './Bai07/bai7.compnent';
 @NgModule({
   declarations: [
     AppComponent,
-    bai3Component, bai5Component, bai6Component
+    bai3Component, bai5Component, bai6Component, bai7Component
   ],
   imports: [
     BrowserModule
