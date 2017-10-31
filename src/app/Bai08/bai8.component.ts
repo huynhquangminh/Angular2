cb5b75b084cbe772a53f1eb443c3127b315f126c
Minh Huynh <huynhtony08@gmail.com>
2017-10-31 21:57:42 +0700

    Angular 2 , tow way binding

---
+++ src/app/Bai08/bai8.component.ts
@@ -0,0 +1,12 @@
+import{Component} from '@angular/core';
+
+
+@Component({
+    selector:'bai08-demo',
+    templateUrl:'bai8.component.html'
+})
+
+export class bai8Component
+{
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {bai3Component} from './Bai3/bai3.component';
 import {bai5Component} from './Bai05/bai5.component';
 import {bai6Component} from './Bai06/bai6.component';
 import {bai7Component} from './Bai07/bai7.compnent';
+import {bai8Component} from './Bai08/bai8.component';
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
-    bai3Component, bai5Component, bai6Component, bai7Component
+    bai3Component, bai5Component, bai6Component, bai7Component, bai8Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
